c1e59a206b1beaeac372ac6092a4633af6c5ac06
VersaLifeHealth <versalife@outlook.com>
2016-05-01 05:05:12 +0200

    Original System (Everything from Tumi)

---
+++ Assests/Populate tbl_dbconn.sql
@@ -0,0 +1,8 @@
+﻿use versalife;
+
+
+insert into tbl_dbconn (usr_id, usr_firstname, usr_password, usr_role) values ('216001', 'Samuel', '001016', '0'); 
+
+insert into tbl_dbconn (usr_id, usr_firstname, usr_password, usr_role) values ('216103', 'Adriella', '103016', '1');
+
+insert into tbl_dbconn (usr_id, usr_firstname, usr_password, usr_role) values ('216003', 'Jerry', '003016', '0');
